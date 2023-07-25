48a1246f03c732eb9dbaa35c6d605f6d3179d9f2
FabianaMelgaco <fabiana.silva@ionsistemas.com.br>
2023-07-25 17:00:11 -0300

    segunda versao inpulse

--- src/tarefas.web/Controllers/TarefaController.cs
+++ src/tarefas.web/Controllers/TarefaController.cs
@@ -11,29 +11,34 @@ namespace tarefas.web.Controllers
         public TarefaController()
         {
             var tarefa1 = new TarefaViewModel();
-            tarefa1.Descricao = "Passear";
-            tarefa1.Titulo = "cachorro";
+            tarefa1.Descricao = "Levar para passear";
+            tarefa1.Titulo = "Cachorro";
+            tarefa1.Status = "Ativo";
             tarefa1.Id = 1;
 
             var tarefa2 = new TarefaViewModel();
             tarefa2.Descricao = "Comprar pao doce";
             tarefa2.Titulo = "Pao";
+            tarefa2.Status = "Ativo";
             tarefa2.Id = 2;
 
             var tarefa3 = new TarefaViewModel();
             tarefa3.Descricao = "Fazer Urgente";
-            tarefa3.Titulo = "O que precisa";
+            tarefa3.Titulo = "Necessidade";
+            tarefa3.Status = "Ativo";
             tarefa3.Id = 3;
 
             var tarefa4 = new TarefaViewModel();
             tarefa4.Descricao = "Proprietario";
-            tarefa4.Titulo = "Quem deve";
+            tarefa4.Titulo = "Responsavel";
+            tarefa4.Status = "Ativo";
             tarefa4.Id = 4;  
 
             listadetarefas.Add(tarefa1);
             listadetarefas.Add(tarefa2);
             listadetarefas.Add(tarefa3);
             listadetarefas.Add(tarefa4);
+      
         }
         public IActionResult Criar()
          {
--- src/tarefas.web/Models/TarefaViewModel.cs
+++ src/tarefas.web/Models/TarefaViewModel.cs
@@ -7,13 +7,12 @@ namespace Tarefas.Web.Models
     public class TarefaViewModel
     {
         public int Id { get; set; }
-        [DisplayName("Descrição")]
+      //  [DisplayName("Descrição")]
         public string Descricao { get; set; }
-        [DisplayName("Título")]
+       // [DisplayName("Título")]
         public string Titulo { get; set; }
        
         // Nova propriedade Status
          public string Status { get; set; }
     }
-    
 }
